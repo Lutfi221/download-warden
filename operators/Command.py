edbb0b898149da16094ca8721233f194fcbf491b
Lutfi Azis <17lutfi21@gmail.com>
2022-09-25 12:00:13 +0700

    feat: improve Command.py

--- operators/Command.py
+++ operators/Command.py
@@ -1,5 +1,6 @@
 
-import os
+import shlex
+import subprocess
 from typing import Callable
 from operators.Operator import Operator
 from utils import expand_variables, print_latest_downloads
@@ -10,7 +11,7 @@ class Command(Operator):
 
     def call(self, downloads: list[str], variables: dict[str, Callable[[], str]]) -> None:
         self.variables = variables
-            
+
         print_latest_downloads(downloads)
 
         while True:
@@ -18,4 +19,17 @@ class Command(Operator):
             if user_input == 'exit':
                 return
             cmd_string = expand_variables(user_input, self.variables)
-            os.popen(cmd_string)
+
+            creationflags = subprocess.CREATE_NO_WINDOW
+
+            if cmd_string[0] == '\t':
+                creationflags = subprocess.CREATE_NEW_CONSOLE
+                cmd_string = 'start cmd /K ' + shlex.quote(cmd_string[1:])
+
+            with subprocess.Popen(cmd_string,
+                                  shell=True,
+                                  stdin=subprocess.PIPE,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE,
+                                  creationflags=creationflags) as p:
+                print(p.stdout.read().decode())
